c95daf7723387097699a6f03a69a2dc81e9751c0
gmarinozzi <gmarinozzi@gmail.com>
2016-02-08 09:41:20 -0600

    oaxaca section info added

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -47,6 +47,24 @@ $(document).ready(function() {
     }
   });
 
+  //modal
+
+  $("#restaurantes").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close, .modal-close-btn").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+
 });
 
 (function (jQuery) {
